e63dabbcc263427d35805909772b7666dc3a3e8e
Ant-Quanique Dancy <adancy@cc-aquanique-mbp.local>
2019-07-07 11:54:07 -0400

    started working on the ability to move the car right and left

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
-    startGame();
+    startGame(400, 750);
   };
 
-  function startGame() {
+  function startGame(canvasWidth,canvasHeight) {
     //grab the canvas from the html
     const canvas = document.getElementById("canvas");
     
@@ -12,38 +12,70 @@ window.onload = function() {
 
     //create the green rectangle background
     ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, 400, 900);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     //creates the grey rectangle
     ctx.fillStyle = 'grey';
-    ctx.fillRect(25, 0, 350, 900);
+    ctx.fillRect(25, 0, canvasWidth-50, canvasHeight);
 
     //creates the white rectangle 
     ctx.fillStyle = 'white';
-    ctx.fillRect(35, 0, 330, 900);
+    ctx.fillRect(35, 0, canvasWidth-70, canvasHeight);
 
     //creates the grey rectangle
     ctx.fillStyle = 'grey';
-    ctx.fillRect(45, 0, 310, 900);
+    ctx.fillRect(45, 0, canvasWidth-90, canvasHeight);
     
     //creates the lane divider
-    for(let i = 10; i < 900; i+=50){
+    for(let i = 10; i < canvasHeight; i+=50){
       ctx.fillStyle = 'white';
-      ctx.fillRect(200, i, 5, 25);
+      ctx.fillRect(canvasWidth/2, i, 5, 25);
 
     }
 
     //creates an image using the image constructor
     let carImg = new Image();
     carImg.src = './images/car.png';
+
+    let carX = 190;
+    let carY = 700;
     
     carImg.addEventListener("load", function() {
-      ctx.drawImage(carImg, 190, 620, 30, 50);
+      ctx.drawImage(carImg, carX, carY, 30, 50);
        }, false);
 
     
-   
+    //image animation steps
+
+    // Clear the canvas
     
+    // Save the canvas state
+
+    //Draw animated shapes
+
+    //Restore the canvas state
+
+    canvas.addEventListener("keydown", keyDownEvent, false);
+
+    function keyDownEvent(e){
+  
+     
+      if(e.keyCode === 37){
+         //player presses the left key
+         if(carX >=45){
+          carX -= 15;
+         }
+      } else if(e.keyCode === 39){
+         //player presses the right key
+        if(carX <= canvasWidth - 45){
+          carX += 15;
+        }
+       
+      }
+    }
+
+
+   
 
 
 
